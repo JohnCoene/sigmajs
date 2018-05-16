97116091e95dd572d67731038cb161862bb5ab5f
JohnCoene <jcoenep@gmail.com>
2018-05-16 17:59:06 +0200

    noverlap added

---
+++ R/noverlap.R
@@ -0,0 +1,53 @@
+#' No overlap
+#'
+#' This plugin runs an algorithm which distributes nodes in the network, ensuring that they do not overlap and providing a margin where specified.
+#'
+#' @param proxy An object of class \code{sigmajsProxy} as returned by \code{\link{sigmajsProxy}}.
+#' @param sg An object of class \code{sigmajs}as intatiated by \code{\link{sigmajs}}.
+#' @param start Whether to start running the noverlap layout.
+#' @param ... any option to pass to the plugin, see \href{https://github.com/jacomyal/sigma.js/tree/master/plugins/sigma.layout.noverlap}{official documentation}.
+#'
+#' @examples
+#' ids <- as.character(1:10)
+#'
+#' nodes <- data.frame(
+#'   id = ids,
+#'   label = LETTERS[1:10],
+#'   size = runif(10, 1, 5),
+#'   stringsAsFactors = FALSE
+#' )
+#'
+#' edges <- data.frame(
+#'   id = 1:15,
+#'   source = sample(ids, 15, replace = TRUE),
+#'   target = sample(ids, 15, replace = TRUE),
+#'   stringsAsFactors = FALSE
+#' )
+#'
+#' sigmajs() %>%
+#'   sg_nodes(nodes, id, label, size) %>%
+#'   sg_edges(edges, id, source, target) %>%
+#'   sg_settings(defaultNodeColor = "#0011ff") %>%
+#'   sg_force() %>%
+#'   sg_noverlap()
+#'
+#' @rdname noverlap
+#' @export
+sg_noverlap <- function(sg, start = TRUE, ...) {
+	sg$x$noverlap <- list(...)
+	sg$x$noverlapStart <- start
+	sg
+}
+
+#' @rdname noverlap
+#' @export
+sg_noverlap_p <- function(proxy) {
+	if (!"sigmajsProxy" %in% class(proxy))
+		stop("must pass sigmajsProxy object", call. = FALSE)
+
+	message <- list(id = proxy$id) # create message
+
+	proxy$session$sendCustomMessage("sg_noverlap_start_p", message)
+
+	return(proxy)
+}
--- inst/htmlwidgets/sigmajs.js
+++ inst/htmlwidgets/sigmajs.js
@@ -19,9 +19,16 @@ HTMLWidgets.widget({
           settings: x.settings
         });
         
-        if(x.hasOwnProperty('force')){
-          s.startForceAtlas2(x.force);
-        }
+				if (x.hasOwnProperty('force')) {
+					s.startForceAtlas2(x.force);
+				}
+
+				if (x.hasOwnProperty('noverlap')) {
+					var noverlap = s.configNoverlap(x.noverlap);
+					if (x.noverlapStart === true) {
+						s.startNoverlap();
+					}
+				}
 
       },
 
@@ -37,6 +44,7 @@ HTMLWidgets.widget({
   }
 });
 
+
 // get chart
 function get_sigma_graph(id) {
 
@@ -51,206 +59,251 @@ function get_sigma_graph(id) {
 	return (s);
 }
 
-// add node
-Shiny.addCustomMessageHandler('sg_add_node_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.addNode(
-				message.data[0]
-			);
-			if (message.refresh === true) {
-				s.refresh();
-			}
-		}
-	}
-);
+// only in shiny
+if (HTMLWidgets.shinyMode) {
 
-// add edge
-Shiny.addCustomMessageHandler('sg_add_edge_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.addEdge(
-				message.data[0]
-			);
-			if (message.refresh === true) {
-				s.refresh();
-			}
-		}
-	}
-);
-
-// restart forceAtlas2
-Shiny.addCustomMessageHandler('sg_force_restart_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			var running = s.isForceAtlas2Running();
-			if (running === true) {
-				s.stopForceAtlas2();
-			}
-			if (running === false) {
-				s.startForceAtlas2(message.data);
+	// add node
+	Shiny.addCustomMessageHandler('sg_add_node_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.addNode(
+					message.data[0]
+				);
 				if (message.refresh === true) {
 					s.refresh();
 				}
 			}
 		}
-	}
-);
+	);
 
-// start forceAtlas2
-Shiny.addCustomMessageHandler('sg_force_start_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			var running = s.isForceAtlas2Running();
-			if (running === false) {
-				s.startForceAtlas2(message.data);
+	// add edge
+	Shiny.addCustomMessageHandler('sg_add_edge_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.addEdge(
+					message.data[0]
+				);
 				if (message.refresh === true) {
 					s.refresh();
 				}
 			}
 		}
-	}
-);
+	);
 
-// stop forceAtlas2
-Shiny.addCustomMessageHandler('sg_force_stop_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			var running = s.isForceAtlas2Running();
-			if (running === true) {
-				s.stopForceAtlas2();
+	// restart forceAtlas2
+	Shiny.addCustomMessageHandler('sg_force_restart_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				var running = s.isForceAtlas2Running();
+				if (running === true) {
+					s.stopForceAtlas2();
+				}
+				if (running === false) {
+					s.startForceAtlas2(message.data);
+					if (message.refresh === true) {
+						s.refresh();
+					}
+				}
 			}
 		}
-	}
-);
+	);
 
-// stop forceAtlas2
-Shiny.addCustomMessageHandler('sg_force_config_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			var running = s.isForceAtlas2Running();
-			if (running === true) {
-				s.configForceAtlas2(message.data);
+	// start forceAtlas2
+	Shiny.addCustomMessageHandler('sg_force_start_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				var running = s.isForceAtlas2Running();
+				if (running === false) {
+					s.startForceAtlas2(message.data);
+					if (message.refresh === true) {
+						s.refresh();
+					}
+				}
 			}
 		}
-	}
-);
+	);
 
-// kill forceAtlas2
-Shiny.addCustomMessageHandler('sg_force_kill_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			var running = s.isForceAtlas2Running();
-			if (running === true) {
-				s.killForceAtlas2();
+	// stop forceAtlas2
+	Shiny.addCustomMessageHandler('sg_force_stop_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				var running = s.isForceAtlas2Running();
+				if (running === true) {
+					s.stopForceAtlas2();
+				}
 			}
 		}
-	}
-);
+	);
 
-// refresh
-Shiny.addCustomMessageHandler('sg_refresh_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.refresh();
+	// stop forceAtlas2
+	Shiny.addCustomMessageHandler('sg_force_config_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				var running = s.isForceAtlas2Running();
+				if (running === true) {
+					s.configForceAtlas2(message.data);
+				}
+			}
 		}
-	}
-);
+	);
 
-// drop node
-Shiny.addCustomMessageHandler('sg_drop_node_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.dropNode(message.data);
-			if (message.refresh === true) {
+	// kill forceAtlas2
+	Shiny.addCustomMessageHandler('sg_force_kill_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				var running = s.isForceAtlas2Running();
+				if (running === true) {
+					s.killForceAtlas2();
+				}
+			}
+		}
+	);
+
+	// refresh
+	Shiny.addCustomMessageHandler('sg_refresh_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
 				s.refresh();
 			}
 		}
-	}
-);
+	);
 
-// drop edge
-Shiny.addCustomMessageHandler('sg_drop_edge_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.dropEdge(message.data);
-			if (message.refresh === true) {
-				s.refresh();
-			}
-		}
-	}
-);
-
-// clear graph
-Shiny.addCustomMessageHandler('sg_clear_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.clear();
-			if (message.refresh === true) {
-				s.refresh();
-			}
-		}
-	}
-);
-
-// kill graph
-Shiny.addCustomMessageHandler('sg_kill_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			s.graph.kill();
-			if (message.refresh === true) {
-				s.refresh();
-			}
-		}
-	}
-);
-
-// add nodes
-Shiny.addCustomMessageHandler('sg_add_nodes_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			message.data.forEach((element) => {
-				s.graph.addNode(element);
-				if (message.refresh === true && message.rate === "iteration") {
+	// drop node
+	Shiny.addCustomMessageHandler('sg_drop_node_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.dropNode(message.data);
+				if (message.refresh === true) {
 					s.refresh();
 				}
-			});
-
-			if (message.refresh === true && message.rate === "once") {
-				s.refresh();
 			}
 		}
-	}
-);
+	);
 
-// add nodes
-Shiny.addCustomMessageHandler('sg_add_edges_p',
-	function (message) {
-		var s = get_sigma_graph(message.id);
-		if (typeof s != 'undefined') {
-			message.data.forEach((element) => {
-				s.graph.addEdge(element);
-				if (message.refresh === true && message.rate === "iteration") {
+	// drop edge
+	Shiny.addCustomMessageHandler('sg_drop_edge_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.dropEdge(message.data);
+				if (message.refresh === true) {
 					s.refresh();
 				}
-			});
-
-			if (message.refresh === true && message.rate === "once") {
-				s.refresh();
 			}
 		}
-	}
-);
+	);
+
+	// clear graph
+	Shiny.addCustomMessageHandler('sg_clear_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.clear();
+				if (message.refresh === true) {
+					s.refresh();
+				}
+			}
+		}
+	);
+
+	// kill graph
+	Shiny.addCustomMessageHandler('sg_kill_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				s.graph.kill();
+				if (message.refresh === true) {
+					s.refresh();
+				}
+			}
+		}
+	);
+
+	// add nodes
+	Shiny.addCustomMessageHandler('sg_add_nodes_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				message.data.forEach((element) => {
+					s.graph.addNode(element);
+					if (message.refresh === true && message.rate === "iteration") {
+						s.refresh();
+					}
+				});
+
+				if (message.refresh === true && message.rate === "once") {
+					s.refresh();
+				}
+			}
+		}
+	);
+
+	// add edges
+	Shiny.addCustomMessageHandler('sg_add_edges_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				message.data.forEach((element) => {
+					s.graph.addEdge(element);
+					if (message.refresh === true && message.rate === "iteration") {
+						s.refresh();
+					}
+				});
+
+				if (message.refresh === true && message.rate === "once") {
+					s.refresh();
+				}
+			}
+		}
+	);
+
+	// add nodes delay
+	Shiny.addCustomMessageHandler('sg_add_nodes_delay_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			if (typeof s != 'undefined') {
+				message.data.forEach((element) => {
+					setTimeout(function () {
+						s.graph.addNode(element);
+						if (message.refresh === true) {
+							s.refresh();
+						}
+					}, element.sigmajsdelay);
+				});
+			}
+		}
+	);
+
+	// add edges delay
+	Shiny.addCustomMessageHandler('sg_add_edges_delay_p',
+		function (message) {
+			var s = get_sigma_graph(message.id);
+			var running = s.isForceAtlas2Running();
+			if (typeof s != 'undefined') {
+				message.data.forEach((element, index) => {
+					setTimeout(function () {
+						s.killForceAtlas2();
+						s.graph.addEdge(element);
+						if (running === true) {
+							s.startForceAtlas2();
+						}
+						if (message.refresh === true) {
+							s.refresh();
+						}
+					}, element.sigmajsdelay);
+				});
+			}
+		}
+	);
+
+}
+
