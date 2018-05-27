2537b17a9ed6e45734354f13bf9f81bb10401c71
JohnCoene <jcoenep@gmail.com>
2018-05-27 21:15:50 +0200

    new sticker

--- data-raw/sticker.R
+++ data-raw/sticker.R
@@ -2,10 +2,12 @@ library(sigmajs)
 library(igraph)
 library(hexSticker)
 
+COLORS <- c('#D8482D','#B30000','#BB100A','#FEF0D9','#B70805','#D44028','#CC301E','#ED7047','#DC5032','#F1784C','#F58051','#BF180F','#D03823','#C72819','#FCA072','#FC8F5C','#E5603D','#E05837')
+
 n <- 50
 ring <- sample_k_regular(n, 5)
 V(ring)$id <- 1:n
-V(ring)$color <- "#c9423f"
+V(ring)$color <- sample(COLORS, n, replace = TRUE)
 
 l <- layout_in_circle(ring)
 
@@ -25,4 +27,6 @@ sticker(
     h_color = "#c9423f",
     p_color = "#c9423f",
     h_fill = "#ffffff",
-    filename="man/figures/logo.png")
+    url = "sigmajs.john-coene.com",
+    u_size = 3,
+    filename="man/figures/logo.png")    
