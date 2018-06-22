1ce2238a6c96ebf4df32424eadceeeb651074f46
JohnCoene <jcoenep@gmail.com>
2018-06-22 17:57:39 +0200

    neighbors

--- inst/htmlwidgets/sigmajs.js
+++ inst/htmlwidgets/sigmajs.js
@@ -1,3 +1,14 @@
+sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index) {
+        neighbors[k] = this.nodesIndex[k];
+    }
+    return neighbors;
+});
+
 HTMLWidgets.widget({
 
   name: 'sigmajs',
@@ -75,6 +86,35 @@ HTMLWidgets.widget({
   				  });
   				}
 				}
+				
+        s.bind('clickNode', function(e) {
+          var nodeId = e.data.node.id,
+              toKeep = s.graph.neighbors(nodeId);
+          toKeep[nodeId] = e.data.node;
+          s.graph.nodes().forEach(function(n) {
+            if (toKeep[n.id])
+              n.color = n.originalColor;
+            else
+              n.color = '#eee';
+          });
+          s.graph.edges().forEach(function(e) {
+            if (toKeep[e.source] && toKeep[e.target])
+              e.color = e.originalColor;
+            else
+              e.color = '#eee';
+          });
+          s.refresh();
+        });
+				
+        s.bind('clickStage', function(e) {
+          s.graph.nodes().forEach(function(n) {
+            n.color = n.originalColor;
+          });
+          s.graph.edges().forEach(function(e) {
+            e.color = e.originalColor;
+          });
+          s.refresh();
+        });
 
 				// start forceAtlas
 				if (x.hasOwnProperty('force')) {
